260bbe1cf7f3c08169fab014f6e9383aeb87fd6d
Oralita <oat431@gmail.com>
2021-10-22 23:38:18 +0700

    [💎 fixed] : login system

--- src/main/java/se331/project/greenlake/config/WebSecurityConfig.java
+++ src/main/java/se331/project/greenlake/config/WebSecurityConfig.java
@@ -63,10 +63,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/auth/**", "/refresh", "/registers/**").permitAll()
-                .antMatchers(HttpMethod.GET,"/events").permitAll()
-                .antMatchers(HttpMethod.GET,"/organizers").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers(HttpMethod.POST,"/events").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET,"/events").permitAll()
+//                .antMatchers(HttpMethod.GET,"/organizers").permitAll()
+//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                .antMatchers(HttpMethod.POST,"/events").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
         // Custom JWT based security filter
